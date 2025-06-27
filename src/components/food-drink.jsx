66dcbbe9e2bec4978bb47d7ea1f1e596a2d117a1
Tony York <tony.york@wnco.com>
2025-06-27 15:00:54 -0500

    patch: Add more catagories

--- src/components/food-drink.jsx
+++ src/components/food-drink.jsx
@@ -1,40 +1,7 @@
 import React from 'react';
+import restaurantsByType from '../data/restaurants.json';
 
 export const FoodDrink = () => {
-  const restaurants = [
-    {
-      name: "Pecan Lodge",
-      type: "BBQ",
-      description: "Award-winning barbecue in Deep Ellum",
-      address: "2702 Main St, Dallas, TX",
-      image: "img/restaurants/pecan-lodge.jpg",
-      bgColor: "#8B4513"
-    },
-    {
-      name: "Knife",
-      type: "Steakhouse", 
-      description: "Upscale dining by John Tesar",
-      address: "5300 E Mockingbird Ln, Dallas, TX",
-      image: "img/restaurants/knife.jpg",
-      bgColor: "#2F4F4F"
-    },
-    {
-      name: "Heimat Bar + Kitchen",
-      type: "German",
-      description: "Modern German cuisine in Bishop Arts",
-      address: "312 W 7th St, Dallas, TX",
-      image: "img/restaurants/heimat.jpg",
-      bgColor: "#B8860B"
-    },
-    {
-      name: "Meridian",
-      type: "Mediterranean",
-      description: "Turkish-Mediterranean flavors in Preston Center",
-      address: "4152 Cole Ave, Dallas, TX",
-      image: "img/restaurants/meridian.jpg",
-      bgColor: "#4682B4"
-    }
-  ];
 
   return (
     <div id="food-drink" className="text-center">
@@ -43,35 +10,50 @@ export const FoodDrink = () => {
           <h2>Food & Drink in Dallas</h2>
           <p>My favorite spots for great food and drinks around the city</p>
         </div>
-        <div className="row">
-          {restaurants.map((restaurant, i) => (
-            <div key={i} className="col-md-6 col-lg-3">
-              <div className="restaurant-item">
-                <div 
-                  className="restaurant-image"
-                  style={{
-                    backgroundColor: restaurant.bgColor,
-                    height: '200px',
-                    borderRadius: '8px',
-                    marginBottom: '15px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: 'white',
-                    fontSize: '18px',
-                    fontWeight: 'bold'
-                  }}
-                >
-                  {restaurant.name}
-                </div>
-                <h4>{restaurant.name}</h4>
-                <span className="restaurant-type">{restaurant.type}</span>
-                <p>{restaurant.description}</p>
-                <small>{restaurant.address}</small>
-              </div>
+        <nav className="cuisine-navigation">
+          <ul className="nav nav-pills nav-justified">
+            {Object.keys(restaurantsByType).map((type) => (
+              <li key={type}>
+                <a href={`#${type.toLowerCase()}`}>{type}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        {Object.entries(restaurantsByType).map(([type, restaurants]) => (
+          <div key={type} className="cuisine-section" id={type.toLowerCase()}>
+            <div className="cuisine-banner">
+              <h3>{type}</h3>
             </div>
-          ))}
-        </div>
+            <div className="row">
+              {restaurants.map((restaurant, i) => (
+                <div key={i} className="col-md-6 col-lg-3">
+                  <div className="restaurant-item">
+                    <div 
+                      className="restaurant-image"
+                      style={{
+                        backgroundColor: restaurant.bgColor,
+                        height: '200px',
+                        borderRadius: '8px',
+                        marginBottom: '15px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: 'white',
+                        fontSize: '18px',
+                        fontWeight: 'bold'
+                      }}
+                    >
+                      {restaurant.name}
+                    </div>
+                    <h4>{restaurant.name}</h4>
+                    <p>{restaurant.description}</p>
+                    <small>{restaurant.address}</small>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
         <div className="row" style={{marginTop: '40px'}}>
           <div className="col-md-12">
             <a href="/" className="btn btn-custom">Back to Home</a>
