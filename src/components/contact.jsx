6686f5b65719db443f4f92c9de7410aab7903bd7
Tony York <tony.york@wnco.com>
2025-06-26 22:13:50 -0500

    fix: turn off warinings for console logs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -18,9 +18,7 @@ export const Contact = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name, email, message);
-    {
-      /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-    }
+    // replace below with your own Service ID, Template ID and Public Key from your EmailJS account
     emailjs
       .sendForm(
         "YOUR_SERVICE_ID",
