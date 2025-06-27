b5a78f449fcb9744b2c23739b00e7f706f3ee83c
Tony York <tony.york@wnco.com>
2025-06-27 09:53:38 -0500

    patch: Update Gallery

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
-
 export const Services = (props) => {
   return (
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <h2>Our Home Services</h2>
+          <p>Making our home yours</p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  {' '}
+                  {" "}
                   <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
@@ -23,7 +18,7 @@ export const Services = (props) => {
                   </div>
                 </div>
               ))
-            : 'loading'}
+            : "loading"}
         </div>
       </div>
     </div>
